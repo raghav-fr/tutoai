ab620c13d9212187435382ab8f8df0f057f602f1
raghav-fr <106470474+raghav-fr@users.noreply.github.com>
2025-05-18 13:09:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -97,7 +97,7 @@ def get_summary():
     data = request.json
     message = data.get('message')
 
-    response = model.generate_content("{message} get me the summary of the conversation in 2 to 3 sentences ")
+    response = model.generate_content(f"{message} get me the summary of the conversation in 2 to 3 sentences ")
     response = response.text
 
     if response:
